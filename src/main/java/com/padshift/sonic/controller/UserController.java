2bf5797229553a16bd23c7deea308a092f6dfaaf
regilkent123 <regilsembrino@gmail.com>
2018-07-12 01:26:28 +0800

    added gracenote api

--- src/main/java/com/padshift/sonic/controller/UserController.java
+++ src/main/java/com/padshift/sonic/controller/UserController.java
@@ -15,6 +15,9 @@ import org.springframework.ui.Model;
 import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import radams.gracenote.webapi.GracenoteException;
+import radams.gracenote.webapi.GracenoteMetadata;
+import radams.gracenote.webapi.GracenoteWebAPI;
 
 import javax.servlet.http.HttpServletRequest;
 import java.io.BufferedReader;
@@ -42,6 +45,11 @@ public class UserController {
     @RequestMapping("/homepage")
     public String showLoginPage(){
         return "loginPage";
+    }
+
+    @RequestMapping("/log")
+    public String login() {
+        return "login";
     }
 
     @RequestMapping("/signup")
@@ -189,6 +197,45 @@ public class UserController {
 
     }
 
+    @RequestMapping("/metadata")
+    public String showmetadata(){
+        String clientID  = "2034677681"; // Put your clientID here.
+        String clientTag = "75917E36EEDFB95B94EC9E68E804B835"; // Put your clientTag here.
+
+        try
+        {
+            /* You first need to register your client information in order to get a userID.
+            Best practice is for an application to call this only once, and then cache the userID in
+            persistent storage, then only use the userID for subsequent API calls. The class will cache
+            it for just this session on your behalf, but you should store it yourself. */
+            GracenoteWebAPI api = new GracenoteWebAPI(clientID, clientTag); // If you have a userID, you can specify it as the third parameter to constructor.
+            String userID = api.register();
+            System.out.println("UserID = " + userID);
+
+            // Once you have the userID, you can search for tracks, artists or albums easily.
+            System.out.println("Search Track:");
+            GracenoteMetadata results = api.searchTrack("Moby", "Play", "Porcelin");
+            results.print();
+
+            System.out.println("Search Artist:");
+            results = api.searchArtist("Moby");
+            results.print();
+
+            System.out.println("Search Album:");
+            results = api.searchAlbum("Moby", "Play");
+            results.print();
+
+            System.out.println("Fetch Album:");
+            results = api.fetchAlbum("97474325-8C600076B380712C6D1C5DC5DC5674F1");
+            results.print();
+        }
+        catch (GracenoteException e)
+        {
+            e.printStackTrace();
+        }
+
+        return "metadata";
+    }
 
 
 
