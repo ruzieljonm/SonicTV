3c38326a74364ead7a902b0249c6cfd662e75fd7
regilkent123 <regilsembrino@gmail.com>
2018-08-06 13:25:39 +0800

    nothing

--- src/main/java/com/padshift/sonic/repository/UserRepository.java
+++ src/main/java/com/padshift/sonic/repository/UserRepository.java
@@ -14,6 +14,4 @@ public interface UserRepository extends JpaRepository<User,Long>  {
     User findByUserNameAndUserPass(String userName, String userPass);
 
     User findByUserName(String userName);
-
-
 }
